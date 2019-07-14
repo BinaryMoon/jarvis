32b97b3c1d6280db3519d26983931b30cb6eda3d
Ben Gillbanks <binarymoon@gmail.com>
2019-07-14 17:19:50 +0100

    Ensure ready.js is concatted last

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -9,8 +9,12 @@ const rename = require( 'gulp-rename' );
 export default function scripts() {
 
 	const destPath = './assets/scripts/';
+	const scriptsSrc = [
+		'./assets/scripts/site/!(ready)*.js',
+		'./assets/scripts/site/ready.js'
+	];
 
-	return src( './assets/scripts/site/*.js' )
+	return src( scriptsSrc )
 		.pipe(
 			concat( 'global.js' )
 		)
