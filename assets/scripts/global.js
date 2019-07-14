a7c05524154963752b4e6e2c5a07ddbe45005dcc
Ben Gillbanks <binarymoon@gmail.com>
2019-07-14 17:20:11 +0100

    build js

--- assets/scripts/global.js
+++ assets/scripts/global.js
@@ -248,61 +248,6 @@ var getParents = function( elem, selector ) {
 	return parents;
 
 };
-/*!
- * The main javascript file for the theme, this makes the magic happen
- *
- * Filename: global.js v1
- *
- * Created by Ben Gillbanks <https://prothemedesign.com/>
- * Available under GPL2 license
- *
- * @package Jarvis
- */
-
-ready(
-	function() {
-
-		events.on(
-			'click',
-			'a[href^="#"]',
-			focusElement
-		);
-
-		// Mobile device detection.
-		var touchClass = 'device-click';
-		if ( is_touch_device() ) {
-			touchClass = 'device-touch';
-		}
-		document.querySelector( 'html' ).classList.add( touchClass );
-
-		menuTouch();
-
-		menuToggle();
-
-		/**
-		 * Add href to links without so that dropdowns work properly on
- 		 * touch devices.
- 		 */
-		events.on(
-			'click',
-			'.menu a:not([href])',
-			function( e ) {
-				e.preventDefault();
-			}
-		);
-
-		// Ensure links with no hrefs don't break the navigation.
-		var menuNoLinks = document.querySelectorAll( 'a:not([href])' );
-
-		menuNoLinks.forEach(
-			function( item, index ) {
-				menuNoLinks[ index ].classList.add( 'menu-no-href' );
-				menuNoLinks[ index ].setAttribute( 'hreg', '#' );
-			}
-		);
-
-	}
-);
 /**
  * JS mobile detection.
  * Is this a touch enabled device or not?
@@ -483,3 +428,58 @@ if ( !Element.prototype.matches ) {
 	}
 
 } )();
+/*!
+ * The main javascript file for the theme, this makes the magic happen
+ *
+ * Filename: global.js v1
+ *
+ * Created by Ben Gillbanks <https://prothemedesign.com/>
+ * Available under GPL2 license
+ *
+ * @package Jarvis
+ */
+
+ready(
+	function() {
+
+		events.on(
+			'click',
+			'a[href^="#"]',
+			focusElement
+		);
+
+		// Mobile device detection.
+		var touchClass = 'device-click';
+		if ( is_touch_device() ) {
+			touchClass = 'device-touch';
+		}
+		document.querySelector( 'html' ).classList.add( touchClass );
+
+		menuTouch();
+
+		menuToggle();
+
+		/**
+		 * Add href to links without so that dropdowns work properly on
+ 		 * touch devices.
+ 		 */
+		events.on(
+			'click',
+			'.menu a:not([href])',
+			function( e ) {
+				e.preventDefault();
+			}
+		);
+
+		// Ensure links with no hrefs don't break the navigation.
+		var menuNoLinks = document.querySelectorAll( 'a:not([href])' );
+
+		menuNoLinks.forEach(
+			function( item, index ) {
+				menuNoLinks[ index ].classList.add( 'menu-no-href' );
+				menuNoLinks[ index ].setAttribute( 'hreg', '#' );
+			}
+		);
+
+	}
+);
