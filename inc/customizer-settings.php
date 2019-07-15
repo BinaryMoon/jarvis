a37b3619616090632c6c5c08d8ed60279870f36d
Ben Gillbanks <binarymoon@gmail.com>
2019-07-15 20:01:54 +0100

    split up the customizer functions
    Only load them when needed

--- inc/customizer-settings.php
+++ inc/customizer-settings.php
@@ -19,6 +19,8 @@ if ( ! class_exists( 'WP_Customize_Control' ) ) {
 
 }
 
+require 'customizer-sanitization.php';
+
 
 /**
  * Theme Customizer properties
@@ -169,88 +171,3 @@ function jarvis_customize_preview_js() {
 }
 
 add_action( 'customize_preview_init', 'jarvis_customize_preview_js' );
-
-
-/**
- * Sanitize checkbox input
- *
- * @param boolean $setting Value to check and sanitize.
- * @return boolean
- */
-function jarvis_sanitize_checkbox( $setting ) {
-
-	return (bool) $setting;
-
-}
-
-
-/**
- * Sanitize category list
- *
- * The list is comma separated. First split the string into items, then loop
- * through all categories and make sure they are ints then join them back
- * together again.
- *
- * @param string $setting Value to check and sanitize.
- * @return string comma separated list of category ids
- */
-function jarvis_sanitize_categories( $setting ) {
-
-	$clean_cats = array();
-	$cats = explode( ',', $setting );
-
-	foreach ( $cats as $c ) {
-		$c = (int) $c;
-
-		if ( $c > 0 ) {
-			$clean_cats[] = $c;
-		}
-	}
-
-	return implode( ',', $clean_cats );
-
-}
-
-
-/**
- * Sanitize the value of an integer.
- *
- * Can be used for dropdown controls, or any other controls where an integer is
- * expected.
- *
- * @param number $setting Value to sanitize.
- * @return integer
- */
-function jarvis_sanitize_int( $setting ) {
-
-	return (int) $setting;
-
-}
-
-
-/**
- * Sanitize colours
- *
- * Would be so much nicer if sanitize_hex_color was available to themes! :)
- *
- * @param string $color Value to sanitize.
- * @return hex|string Returns clean colour, or empty string if not a valid colour.
- */
-function jarvis_sanitize_hex_color( $color ) {
-
-	if ( '' === $color ) {
-
-		return '';
-
-	}
-
-	// 3 or 6 hex digits, or the empty string.
-	if ( preg_match( '|^#([A-Fa-f0-9]{3}){1,2}$|', $color ) ) {
-
-		return $color;
-
-	}
-
-	return '';
-
-}
