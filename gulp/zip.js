164cdd54cce7486f1941e1e6ebb00bda00671338
Ben Gillbanks <binarymoon@gmail.com>
2019-07-17 20:52:47 +0100

    update zip build to exclude new js directories

--- gulp/zip.js
+++ gulp/zip.js
@@ -14,10 +14,12 @@ const compress = function() {
 		'!node_modules/**',
 		'!gulp/',
 		'!gulp/**',
-		'!assets/scripts/src/',
-		'!assets/scripts/src/**',
-		'!assets/scripts-customizer/src/',
-		'!assets/scripts-customizer/src/**',
+		'!assets/scripts/src-global/',
+		'!assets/scripts/src-global/**',
+		'!assets/scripts/src-customizer-preview/',
+		'!assets/scripts/src-customizer-preview/**',
+		'!assets/scripts/src-customizer-controls/',
+		'!assets/scripts/src-customizer-controls/**',
 		'!**/*.scss',
 	];
 
