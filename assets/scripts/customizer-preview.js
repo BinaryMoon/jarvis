a95ff826ac1400692526e6162e8f79c093a9c326
Ben Gillbanks <binarymoon@gmail.com>
2019-07-17 21:55:09 +0100

    Change the approach to the customizer-preview css class
    It's now added to the html element with javascript from within the customizer.
    
    This makes the css take effect properly, and removes the php overhead of additional conditonals (every little helps!)

--- assets/scripts/customizer-preview.js
+++ assets/scripts/customizer-preview.js
@@ -141,6 +141,17 @@
 } )( jQuery );
 
 /**
+ * Add a customizer-preview class to the html element.
+ *
+ * This is used to prevent the smooth scrolling from working so that we don't
+ * get lots of jumping around when elements are updated.
+ */
+; ( function( $ ) {
+
+	$( 'html' ).addClass( 'customizer-preview' );
+
+} )( jQuery );
+/**
  * Live updates for the header text colour.
  */
 ; ( function( $ ) {
