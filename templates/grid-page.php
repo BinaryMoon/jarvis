492b4053cb74c5f567201dccadb05a866f69e742
Ben Gillbanks <binarymoon@gmail.com>
2019-07-17 21:17:19 +0100

    Fix @package info

--- templates/grid-page.php
+++ templates/grid-page.php
@@ -6,7 +6,7 @@
  *
  * Template Name: Child Page Grid
  *
- * @package Granule
+ * @package Jarvis
  * @subpackage PageTemplate
  * @author Ben Gillbanks <ben@prothemedesign.com>
  * @license http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
