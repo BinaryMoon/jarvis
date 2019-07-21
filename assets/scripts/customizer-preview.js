3c91e7fe0dc9ad5a45a25eeb8c4abcb0655fca45
Ben Gillbanks <binarymoon@gmail.com>
2019-07-21 22:18:12 +0100

    build scripts

--- assets/scripts/customizer-preview.js
+++ assets/scripts/customizer-preview.js
@@ -199,6 +199,13 @@
 		'preview-ready',
 		function() {
 
+			/**
+			 * Set default values.
+			 */
+			$( '.byline' ).css( 'display', wp.customize( 'jarvis_single_show_author' )() ? 'inline' : 'none' );
+			$( '.posted-on' ).css( 'display', wp.customize( 'jarvis_single_show_date' )() ? 'inline' : 'none' );
+			$( '.content-single .contributor' ).css( 'display', wp.customize( 'jarvis_single_show_author_details' )() ? 'grid' : 'none' );
+
 			// Edit Single Post Layout.
 			wp.customize(
 				'jarvis_single_layout',
@@ -218,6 +225,7 @@
 			wp.customize(
 				'jarvis_single_show_author',
 				function( value ) {
+
 					value.bind(
 						function( to ) {
 
@@ -231,10 +239,27 @@
 			wp.customize(
 				'jarvis_single_show_date',
 				function( value ) {
+
 					value.bind(
 						function( to ) {
 
 							$( '.posted-on' ).css( 'display', to ? 'inline' : 'none' );
+
+						}
+					);
+				}
+			);
+
+			wp.customize(
+				'jarvis_single_show_author_details',
+				function( value ) {
+
+					// $( '.content-single .contributor' ).css( 'display', to ? 'grid' : 'none' );
+
+					value.bind(
+						function( to ) {
+
+							$( '.content-single .contributor' ).css( 'display', to ? 'grid' : 'none' );
 
 						}
 					);
