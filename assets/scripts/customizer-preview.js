8cf361d94c7f682bb0a5743c70d2039d6183b22b
Ben Gillbanks <binarymoon@gmail.com>
2019-07-22 10:30:02 +0100

    build assets

--- assets/scripts/customizer-preview.js
+++ assets/scripts/customizer-preview.js
@@ -270,6 +270,43 @@
 
 } )( jQuery );
 /**
+ * Live updates for the site description.
+ */
+; ( function( $ ) {
+
+	wp.customize.bind(
+		'preview-ready',
+		function() {
+
+			// Edit Header Layout.
+			wp.customize(
+				'jarvis_header_layout',
+				function( value ) {
+					value.bind(
+						function( to ) {
+
+							var count = 1;
+							var selectors = '';
+
+							for ( i = 0; i <= count; i++ ) {
+								selectors += ' header-layout-' + i;
+							}
+
+							$( 'body' )
+								.removeClass( selectors )
+								.addClass( 'header-layout-' + to );
+
+						}
+					);
+				}
+			);
+
+
+		}
+	);
+
+} )( jQuery );
+/**
  * Add a customizer-preview class to the html element.
  *
  * This is used to prevent the smooth scrolling from working so that we don't
