72769fd1394d8ab5a131f41f5046d6a7a44f35d9
Ben Gillbanks <binarymoon@gmail.com>
2019-07-23 20:03:22 +0100

    Add a full-width page template

---
+++ templates/full-width.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Full-width page template
+ *
+ * Stretch across the width of the page.
+ * The template loads the normal single template. All the changes are made in
+ * the sass/theme/_single.scss file using the body class as a modifier.
+ *
+ * Template Name: Full-width
+ *
+ * @package Jarvis
+ * @subpackage PageTemplate
+ * @author Ben Gillbanks <ben@prothemedesign.com>
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU Public License
+ */
+
+require get_theme_file_path( 'singular.php' );
