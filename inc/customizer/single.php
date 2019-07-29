3234f0a4779e273057f4bbda78e50ad8900550f4
Ben Gillbanks <binarymoon@gmail.com>
2019-07-29 20:54:06 +0100

    Add a setting that toggles featured image usage

--- inc/customizer/single.php
+++ inc/customizer/single.php
@@ -94,6 +94,30 @@ function jarvis_customizer_single( WP_Customize_Manager $wp_customize ) {
 	);
 
 	/**
+	 * Setting to use the featured image as a header image.
+	 *
+	 * Unfortunately I have yet to work out a way to make this a function that
+	 * can be live previewed.
+	 */
+	$wp_customize->add_setting(
+		'jarvis_single_header',
+		array(
+			'default' => false,
+			'capability' => 'edit_theme_options',
+			'sanitize_callback' => 'jarvis_sanitize_checkbox',
+		)
+	);
+
+	$wp_customize->add_control(
+		'jarvis_single_header',
+		array(
+			'label' => esc_html__( 'Use Featured Image as header image.', 'jarvis' ),
+			'section' => 'jarvis_single',
+			'type' => 'checkbox',
+		)
+	);
+
+	/**
 	 * Setting to show/ hide the post date.
 	 */
 	$wp_customize->add_setting(
