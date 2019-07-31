027b1a8308bb9e195c05ed1bb4f6db5bb94b887d
Ben Gillbanks <binarymoon@gmail.com>
2019-07-31 20:37:18 +0100

    build styles and scripts before watching files

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -42,13 +42,18 @@ export const buildTOC = toc;
 export const buildCritical = criticalCSS;
 export const buildSVG = optimizeSVG;
 
-export const watchFiles = cb => {
+export const watchFiles = function() {
 	watch( [ '*.scss', './assets/sass/**/*.scss' ], series( parallel( styles, editor_styles, editor_blocks ), rtl, toc ) );
 	watch( './assets/sass/customizer/*.scss', customizer_styles );
 	watch( './assets/scripts/src-global/*.js', scriptsGlobal );
 	watch( './assets/scripts/src-customizer-preview/*.js', customizerPreview );
 	watch( './assets/scripts/src-customizer-controls/*.js', customizerControls );
 	watch( './assets/svg/src/*.svg', optimizeSVG );
+	return;
 };
 
-export default watchFiles;
+export default series(
+	buildStyles,
+	buildScripts,
+	watchFiles
+);
