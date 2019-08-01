09186f43f568caa5a52c80dc90b04c7dceed5fa4
Ben Gillbanks <binarymoon@gmail.com>
2019-08-01 08:40:00 +0100

    rename menu_id for main navigation

--- parts/navigation.php
+++ parts/navigation.php
@@ -30,7 +30,7 @@ if ( ! has_nav_menu( 'menu-1' ) ) {
 	wp_nav_menu(
 		array(
 			'theme_location' => 'menu-1',
-			'menu_id' => 'nav',
+			'menu_id' => 'menu-primary',
 			'menu_class' => 'menu-wrap',
 			'container' => false,
 			'item_spacing' => 'discard',
