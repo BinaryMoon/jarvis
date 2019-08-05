39d1ac16ab41a827a14c71fe1a16f498709a464f
Ben Gillbanks <binarymoon@gmail.com>
2019-08-05 23:29:17 +0100

    Revert change to contributor name, to ensure it displays correctly

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -41,7 +41,7 @@ function jarvis_post_author() {
 	}
 
 	return sprintf(
-		'<span class="byline meta author vcard"><a class="url fn n p-name u-url" href="%s">%s</a></span>',
+		'<span class="byline meta author v-card"><a class="url fn n p-name u-url" href="%s">%s</a></span>',
 		esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ),
 		esc_html( get_the_author() )
 	);
@@ -165,7 +165,11 @@ function jarvis_contributor( $user_id = null, $post_count = null ) {
 
 		<div class="entry">
 
-			<h2><?php echo jarvis_post_author(); // phpcs: ignore.  ?></h2>
+			<h2 class="byline meta author v-card">
+				<a class="url fn n p-name u-url" href="<?php echo esc_url( get_author_posts_url( $user_id ) ); ?>">
+					<?php the_author_meta( 'display_name', $user_id ); ?>
+				</a>
+			</h2>
 
 <?php
 
