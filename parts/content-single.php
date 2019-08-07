34c5e9c7fe157320b6ff300f6f0a7623360e1978
Ben Gillbanks <binarymoon@gmail.com>
2019-08-07 23:01:41 +0100

    Change a word for a bit of personality

--- parts/content-single.php
+++ parts/content-single.php
@@ -63,7 +63,7 @@
 		array(
 			'next_text' => '<span class="meta-nav" aria-hidden="true">' . esc_html__( 'Next', 'jarvis' ) . '</span> ' .
 				'<span class="post-title">%title</span>',
-			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . esc_html__( 'Previous', 'jarvis' ) . '</span> ' .
+			'prev_text' => '<span class="meta-nav" aria-hidden="true">' . esc_html__( 'Previously', 'jarvis' ) . '</span> ' .
 				'<span class="post-title">%title</span>',
 		)
 	);
