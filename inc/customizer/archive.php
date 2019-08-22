63630148501aba3b006079f7f4d1b5d11fc8fb6d
Ben Gillbanks <binarymoon@gmail.com>
2019-08-22 20:09:24 +0100

    Correct archive post layout title
    Was using the Header Height title originally (copy and paste error :))

--- inc/customizer/archive.php
+++ inc/customizer/archive.php
@@ -109,7 +109,7 @@ function jarvis_customizer_archive( WP_Customize_Manager $wp_customize ) {
 	$wp_customize->add_control(
 		'jarvis_archive_articles',
 		array(
-			'label' => esc_html__( 'Header Height', 'jarvis' ),
+			'label' => esc_html__( 'Archive Post Layout', 'jarvis' ),
 			'section' => 'jarvis_archive',
 			'type' => 'radio',
 			'choices' => array(
