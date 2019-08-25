b9062446eb13dcf319edd6ee1aad545204580a23
Ben Gillbanks <binarymoon@gmail.com>
2019-08-25 22:28:35 +0100

    build styles

--- assets/scripts/customizer-preview.js
+++ assets/scripts/customizer-preview.js
@@ -361,6 +361,29 @@
 				}
 			);
 
+			// Edit Header Layout.
+			wp.customize(
+				'jarvis_header_border',
+				function( value ) {
+					value.bind(
+						function( to ) {
+
+							var count = 3;
+							var selectors = '';
+
+							for ( i = 0; i <= count; i++ ) {
+								selectors += ' header-border-' + i;
+							}
+
+							$( 'body' )
+								.removeClass( selectors )
+								.addClass( 'header-border-' + to );
+
+						}
+					);
+				}
+			);
+
 			// Edit Archive Header Height.
 			wp.customize(
 				'jarvis_archive_header_height',
