0b26c1584bde1e0cf72cc1fcd97c752e31c35776
Ben Gillbanks <binarymoon@gmail.com>
2019-10-12 20:31:19 +0100

    update phpstan and add tgmpa stub

---
+++ tests/tgmpa-stubs.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Helper function to register a collection of required plugins.
+ *
+ * @param array $plugins An array of plugin arrays.
+ * @param array $config  Optional. An array of configuration values.
+ */
+function tgmpa( $plugins, $config = array() ) {
+}
